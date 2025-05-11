9e9b2b75e1c2bfb596ddace0d2d5d630d709860d
Ravesh Rawal <101560639+raveshrawal@users.noreply.github.com>
2025-05-11 15:00:45 +0530

    Time: 5 ms (42.11%), Space: 44.8 MB (28.61%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
@@ -1,23 +1,25 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-        
+        // [[3,6],[1,5],[4,7]]
+        //  ✅  ✅ ✅ ✅  ✅  ✅  => Count of number included in interval = 7
+        //           3   4   5   6
+        //   1   2   3   4   5
+        //               4   5   6   7
+
+        // [[1,3],[5,8]]
+        //  ✅  ✅ ✅ ❌ ✅  ✅ ✅ ✅ => Count of number included in interval = 7
+        //           
+        //   1   2   3   
+        //                  5   6   7   8 
+
         HashSet<Integer> hs = new HashSet<>();
-
         for(int i=0;i<nums.size();i++){
-
             int start = nums.get(i).get(0);
             int end = nums.get(i).get(1);
-
             while(start<=end){
                 hs.add(start++);
             }
-
         }
         return hs.size();
     }
 }
-
-
- //           3   4   5   6
- //   1   2   3   4   5
- //               4   5   6   7
