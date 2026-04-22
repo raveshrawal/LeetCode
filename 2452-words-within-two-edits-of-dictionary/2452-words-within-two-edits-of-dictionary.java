cf8d2fe627d5d4cb66632f63d54ce7a14fdc5e6c
Ravesh Rawal <101560639+raveshrawal@users.noreply.github.com>
2026-04-22 15:17:27 +0530

    Time: 2 ms (97.79%), Space: 44.1 MB (92.65%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -1,39 +1,29 @@
+import java.util.*;
+
 class Solution {
-    public boolean compareDifference(String word1, String word2) {
-
-        if (word1.length() != word2.length()) {
-            return false;
-        }
-
-        int diff = 0;
-        char[] w1 = word1.toCharArray();
-        char[] w2 = word2.toCharArray();
-
-        for (int i = 0; i < w1.length; i++) {
-            if (w1[i] != w2[i]) {
-                diff++;
-            }
-            if (diff > 2) {
-                return false;
-            }
-        }
-        return true;
-
-    }
-
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
 
-        List<String> res = new ArrayList<String>();
-
-        for (int i = 0; i < queries.length; i++) {
-            for (int j = 0; j < dictionary.length; j++) {
-                if(compareDifference(queries[i], dictionary[j])){
-                    res.add(queries[i]);
+        for (String q : queries) {
+            for (String d : dictionary) {
+                if (diffAtMostTwo(q, d)) {
+                    result.add(q);
                     break;
                 }
             }
         }
-        return res;
 
+        return result;
+    }
+
+    private boolean diffAtMostTwo(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diff++;
+                if (diff > 2) return false;
+            }
+        }
+        return true;
     }
 }
